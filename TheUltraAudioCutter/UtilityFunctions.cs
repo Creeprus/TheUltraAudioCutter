a62a711afd138e650903393a72e7549e44c1d909
Creeprus <leva.kornienko@yandex.ru>
2022-06-06 16:45:42 +0300

    Fixed Install issue
    Now to download the appm you should  run "Add-AppDevPackage.ps1" with PowerShell (use "InstallFolder" link)

---
+++ TheUltraAudioCutter/UtilityFunctions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace AudioMerger
+{
+    class UtilityFunctions
+    {
+        /// <summary>
+        ///Удаляет папку
+        /// </summary>
+        /// <param name="pPath">Удаляемая директория</param>
+        /// <returns>Пустая строка, при успехе</returns>
+        public static String DeleteDir(String pPath)
+        {
+            String retval = "";
+
+            if (Directory.Exists(pPath))
+            {
+                try
+                {
+
+                    String[] filenames = Directory.GetFiles(pPath);
+                    foreach (String filename in filenames)
+                        File.Delete(filename);
+
+                    Directory.Delete(pPath, true);
+                }
+                catch (System.Exception exc)
+                {
+                    retval = exc.Message;
+                }
+            }
+
+            return retval;
+        }
+        /// <summary>
+        /// Удаляет файлы из директории
+        /// </summary>
+        /// <param name="pPath"> Путь до папки</param>
+        /// <returns> Пустую строку, при успехе</returns>
+        public static String DeleteFilesFromDir(String pPath)
+        {
+            String retval = "";
+
+            if (Directory.Exists(pPath))
+            {
+                try
+                {
+
+                    String[] filenames = Directory.GetFiles(pPath);
+                    foreach (String filename in filenames)
+                        File.Delete(filename);
+
+                }
+                catch (System.Exception exc)
+                {
+                    retval = exc.Message;
+                }
+            }
+
+            return retval;
+        }
+    }
+}
